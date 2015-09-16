ac65d084416a3a1df08f4296f1119ffa13e89164
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2015-09-16 14:32:38 -0400

    Minor change to terms of awesome

--- app/views/structure/footer.blade.php
+++ app/views/structure/footer.blade.php
@@ -1,5 +1,5 @@
 <p> &nbsp; </p>
-<p align="center"><a href="{{URL::route('privacy_policy')}}">Privacy Policy</a> | <a href="{{URL::route('terms_of_service')}}">Terms of Service</a></p>
+<p align="center"><a href="{{URL::route('privacy_policy')}}">Privacy Policy</a> | <a href="{{URL::route('terms_of_service')}}">Terms of Awesome</a></p>
 <p align="center"> &copy; Copyright 2015, DemHUB Inc. All Rights Reserved.</p>
 </body>
 </html>
