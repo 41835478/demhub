d5c2a59595d1662c43b3da6ea54d2de424ce4e8d
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2015-09-16 14:29:14 -0400

    Minor footer change

--- app/views/structure/footer.blade.php
+++ app/views/structure/footer.blade.php
@@ -1,5 +1,7 @@
-<p> &nbsp; </p>
-<p align="center"><a href="{{URL::route('privacy_policy')}}">Privacy Policy</a> | <a href="{{URL::route('terms_of_service')}}">Terms of Service</a></p>
-<p align="center"> &copy; Copyright 2015, DemHUB Inc. All Rights Reserved.</p>
+<footer>
+  <p> &nbsp; </p>
+  <p align="center"><a href="{{URL::route('privacy_policy')}}">Privacy Policy</a> | <a href="{{URL::route('terms_of_service')}}">Terms of Service</a></p>
+  <p align="center"> &copy; Copyright 2015, DemHUB Inc. All Rights Reserved.</p>
+</footer>
 </body>
 </html>
