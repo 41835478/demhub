5de4580fd21a18204dd90e2e0ebfa04cefe58ff3
lhaggarty <lhaggarty@me.com>
2015-09-23 13:41:27 -0400

    Update About Us Page

--- app/views/about-us.blade.php
+++ app/views/about-us.blade.php
@@ -15,19 +15,49 @@
 
 		We are committed to everything we do as it impacts more than just our team, it impacts our communities. So we make sure we're reliable, collaborative, and inclusive, providing the best platform for networking and knowledge sharing.
 	</p>
-	
+	<br>
 </div>
 </div>
 <div class="row">
 	<div class="col-md-12">
-	<div class="col-xs-2 col-xs-offset-1">
+	<div class="col-xs-2 col-xs-offset-2">
 		<img src="./images/photos/jenholmes_bio_pic.png" class="img-responsive">
 	</div>	
-	<div class="col-xs-8 col-xs-offset-1" style="text-align:left">	
+	<div class="col-xs-5 col-xs-offset-1" style="text-align:left">	
 	<h3>Jennifer Duke Holmes, Founder, President </h3>	
 	<p>
 		Jennifer has spent over 5 years in the Disaster and Emergency Management field as both an academic and an entrepreneur. She founded Hybrid Intuition Media in 2013 which develops digital media materials within an emergency management context. Jen has also been President of the global student membership for the International Association of Emergency Managers, the world’s largest association of its kind.
 	</p>
 	</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+	<div class="col-xs-2 col-xs-offset-2">
+		<img src="./images/photos/SeanKondra_bio_pic.jpg" class="img-responsive">
+	</div>	
+	<div class="col-xs-5 col-xs-offset-1" style="text-align:left">	
+	<h3>Sean William Kondra, VP Operations & Corporate Development</h3>	
+	<p>
+		Sean has spent over 16 years in various pillars of the investment industry. He has spent time as an investment advisor, an investor communications director and, most recently, was the co- founder of a venture capital/corporate consulting corporation. Sean’s passion is assisting entrepreneurs and inventors at the genesis of an idea in commercializing their vision and creating successful ventures. 
+	</p>
+	</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+	<div class="col-xs-2 col-xs-offset-2">
+		<img src="./images/photos/leonhaggarty_bio_pic.png" class="img-responsive">
+	</div>	
+	<div class="col-xs-5 col-xs-offset-1" style="text-align:left">	
+	<h3>Leon Haggarty, Lead Developer</h3>	
+	<p>
+		Jennifer has spent over 5 years in the Disaster and Emergency Management field as both an academic and an entrepreneur. She founded Hybrid Intuition Media in 2013 which develops digital media materials within an emergency management context. Jen has also been President of the global student membership for the International Association of Emergency Managers, the world’s largest association of its kind.
+	</p>
+	</div>
+	</div>
+</div>
 	<hr>
 @endsection('content')
