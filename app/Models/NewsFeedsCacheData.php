9809c1c8d50df77148477526d4746ccaf9f586d2
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2015-10-12 13:41:24 -0400

    Add news feed cache data model

---
+++ app/Models/NewsFeedsCacheData.php
@@ -0,0 +1,14 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsFeedsCacheData extends Model
+{
+  /**
+   * Get the news feed record associated with the cache data.
+   */
+  // public function newsFeed()
+  // {
+  //   return $this->...('App\Models\NewsFeed');
+  // }
+}
