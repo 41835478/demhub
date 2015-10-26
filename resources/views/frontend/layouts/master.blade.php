8a7335096d295e68f4bda579b3d3b9627b488e69
lhaggarty <lhaggarty@me.com>
2015-10-25 20:07:50 -0400

    adding google analytics to layout master view page

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -65,5 +65,16 @@
         @yield('after-scripts-end')
 
         @include('includes.partials.ga')
+        <!-- google-analytics -->
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-69289302-1', 'auto');
+        ga('send', 'pageview');
+
+</script>
     </body>
 </html>
--- resources/views/frontend/user/menu-user/second-menu-user.blade.php
+++ resources/views/frontend/user/menu-user/second-menu-user.blade.php
@@ -14,7 +14,7 @@
 
 				 <li>
 
-				 
+
 				<a href="" style=""> DISCUSSION - COMING SOON</a></li>
 				 @if(Request::url() === url('resource_filter'))
 				 <li class="active">
@@ -23,10 +23,10 @@
 				 @endif
 				<a href="{{url('resource_filter')}}"> RESOURCES</a></li>
 
-  	            <li><a href="" style="">EVENTS - COMING SOON</a></li>
+  	            <li><a href="" style="">MAP - COMING SOON</a></li>
 
   				<!-- <li><a href="{url('logout')}}"><img src="css/share-image-black.png" class="blackImage" alt="Hot Potato" width="55" height="21" style=""><img src="css/share-image-white.png" class="whiteImage" alt="Hot Potato" width="55" height="21" style=""> Feed</a></li> -->
-  				<li><a href="" style="border-right:2px solid #fff">MEDIA - COMING SOON</a></li>
+  				<li><a href="" style="border-right:2px solid #fff">CONNECTIONS - COMING SOON</a></li>
 
   	            <!-- <li role="presentation"><a href="{url('logout')}}">About &amp; Contact</a></li> -->
   	      </div>
