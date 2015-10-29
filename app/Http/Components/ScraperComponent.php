c1b63e3c5bc830c4aedb10345c71441f68560c1e
Poya <poya.rahmati@gmail.com>
2015-10-28 21:25:02 -0400

    Added primary function to determine keywords and division_ids from article text

--- app/Http/Components/ScraperComponent.php
+++ app/Http/Components/ScraperComponent.php
@@ -8,6 +8,7 @@
 
 namespace App\Http\Components;
 
+use App\Models\ArticleDetail;
 use Nathanmac\Utilities\Parser\Parser;
 use App\Models\Article;
 use App\Models\Keyword;
@@ -21,6 +22,94 @@ class ScraperComponent
 	const itemTypeNewsArticle = 1;
 	const itemTypeScientificPaper = 2;
 	const itemTypeOther = 9;
+
+
+
+	////////////////////////////
+	////      HELPERS
+	////////////////////////////
+
+	/**
+	 * This function takes in a string and tries to determine related tags and divisions based on a set of provided keyword models
+	 *
+	 * @param string $text Text to be analyzed
+	 * @param /App/Models/Keyword $all_keywords A complete list of keywords models available (this is to speed up processing when dealing with many keywords)
+	 * @return array containing a sorted arrays for keywords as well as divisions
+	 */
+	public static function guessKeywords($text, $all_keywords)
+	{
+		$sensitivity = 1;		// min number of occurances required for a keyword/division to be returned
+		$result = array();
+		$all_divisions = array();
+		$divisions_raw = array();
+		$keywords = array();
+		$divisions = array();
+
+		//black lists
+		$ignore = array("am","you","is","are","i","they","them","they're","it","it's","its","you're","i'm","&");
+		$remove = array("!",",",":",";","@","#","?","(",")","*",".","\"","/",'"',"%","&");
+
+		//break string into words and remove duplicates
+		//$words = array_unique(explode(' ', str_replace($remove, " ", strtolower($text))));
+		$words = explode(' ', str_replace($remove, " ", strtolower($text)));
+
+		//pre-set the categories array
+		foreach($all_keywords as $i=>$key){
+			if(!isset($result[$key->keyword])){
+				$result[$key->keyword] = 0;
+				$all_divisions[$key->keyword] = self::convertDBStringToArray($key->divisions);
+			}
+		}
+
+		//go through all words
+		foreach ($words as $wi=>$word)
+		{
+			//remove extra characters
+			//$word = str_replace($remove, "", $word);
+
+			//check ignore list
+			if(!in_array($word, $ignore))
+			{
+
+				$phrase_double1 = isset($words[$wi+1]) ? $word.' '.$words[$wi+1] : null;
+				$phrase_double2 = isset($words[$wi-1]) ? $words[$wi-1].' '.$word : null;
+				$phrase_triple1 = isset($words[$wi+1])&&isset($words[$wi+2]) ? $word.' '.$words[$wi+1].' '.$words[$wi+2] : null;
+				$phrase_triple2 = isset($words[$wi-1])&&isset($words[$wi+1]) ? $words[$wi-1].' '.$word.' '.$words[$wi+1] : null;
+				$phrase_triple3 = isset($words[$wi-1])&&isset($words[$wi-2]) ? $words[$wi-2].' '.$words[$wi-1].' '.$word : null;
+				$temp_divs = array();
+
+				if(isset($result[$word])){ $result[$word]++; $temp_divs = $all_divisions[$word]; }
+				elseif($phrase_double1 != null && isset($result[$phrase_double1])){ $result[$phrase_double1]++; $temp_divs = $all_divisions[$phrase_double1]; }
+				elseif($phrase_double2 != null && isset($result[$phrase_double2])){ $result[$phrase_double2]++; $temp_divs = $all_divisions[$phrase_double2]; }
+				elseif($phrase_triple1 != null && isset($result[$phrase_triple1])){ $result[$phrase_triple1]++; $temp_divs = $all_divisions[$phrase_triple1]; }
+				elseif($phrase_triple2 != null && isset($result[$phrase_triple2])){ $result[$phrase_triple2]++; $temp_divs = $all_divisions[$phrase_triple2]; }
+				elseif($phrase_triple3 != null && isset($result[$phrase_triple3])){ $result[$phrase_triple3]++; $temp_divs = $all_divisions[$phrase_triple3]; }
+
+				foreach($temp_divs as $div)
+				{
+					if(!isset($divisions_raw[$div]))
+						$divisions_raw[$div] = 1;
+					else
+						$divisions_raw[$div]++;
+				}
+
+			}
+		}
+
+		arsort($result);
+		foreach ($result as $i=>$r){
+			if($r >= $sensitivity) $keywords[] = $i;
+		}
+		//if(empty($keywords)) $keywords[] = NULL;
+
+		arsort($divisions_raw);
+		array_slice($divisions_raw, 0, 3); 				//allow up to 2 only
+		foreach ($divisions_raw as $i=>$r){
+			if($r >= $sensitivity) $divisions[] = $i;
+		}
+
+		return array('keywords'=>$keywords, 'divisions'=>$divisions);
+	}
 
 	/**
 	 * Standard function to convert an int/string array to format optimized for search needed to be stored in db
