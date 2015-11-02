50767ea69ce1fda698c0dd07353fea4240e83c6c
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2015-11-02 17:19:12 -0500

    Fix user dashboard menu

--- resources/views/frontend/user/dashboard/connections.blade.php
+++ resources/views/frontend/user/dashboard/connections.blade.php
@@ -5,11 +5,11 @@
 <div id="user-settings" class="row" style="padding-top:50px;padding-left:60px">
 
 		<div class="col-md-2" style="background: rgba(0,0,0,0.85);padding-bottom:50px;">
-			@include('frontend.user.user_dashboard.menu._dashboard_menu')
+			@include('frontend.user.dashboard.menu._dashboard_menu')
 		</div>
     	<div style="padding-top:50px">
 
-    		
+
 		</div>
 </div>
 @endsection('content')
--- resources/views/frontend/user/dashboard/menu/_dashboard_menu.blade.php
+++ resources/views/frontend/user/dashboard/menu/_dashboard_menu.blade.php
@@ -1,34 +1,43 @@
+<ul class="nav nav-stacked navbar-inverse" id="user-function">
 
-      <ul class="nav nav-stacked navbar-inverse" id="user-function">
+    @if(Request::url() === url('dashboard'))
+      <li class="active">
+    @else
+      <li>
+    @endif
+      <a href="{{url('dashboard')}}"><i class="fa fa-user"></i> PROFILE</a>
+    </li>
 
+    @if(Request::url() === url('publications'))
+      <li class="active">
+    @else
+      <li>
+    @endif
+      <a href="{{url('publications')}}"><i class="fa fa-briefcase"></i> PUBLICATIONS</a>
+    </li>
 
-                @if(Request::url() === url('dashboard'))
-                <li class="active">
-                @else
-                <li>
-                @endif
-                    <a href="{{url('dashboard')}}"><i class="fa fa-user"></i> PROFILE</a>
-                </li>
+    @if(Request::url() === url('connections'))
+      <li class="active">
+    @else
+      <li>
+    @endif
+      <a href=""><i class="fa fa-users"></i> CONNECTIONS - COMING SOON</a>
+    </li>
 
-                @if(Request::url() === url('connections'))
-                <li class="active">
-                @else
-                <li>
-                @endif
-                    <a href="{{url('connections') }}"><i class="fa fa-user"></i> CONNECTIONS</a>
-                </li>
-                @if(Request::url() === url('privacy_settings'))
-                <li class="active">
-                @else
-                <li>
-                @endif
-                    <a href=""><i class="fa fa-globe" style=""></i> PRIVACY SETTINGS - COMING SOON</a>
-                </li>
-                @if(Request::url() === url('privacy_settings'))
-                <li class="active">
-                @else
-                <li>
-                @endif
-                    <a href=""><i class="fa fa-file" style=""></i> COLLECTION - COMING SOON</a>
-                </li>
-			</ul>
+    @if(Request::url() === url('privacy_settings'))
+      <li class="active">
+    @else
+      <li>
+    @endif
+      <a href=""><i class="fa fa-globe" style=""></i> PRIVACY SETTINGS - COMING SOON</a>
+    </li>
+
+    @if(Request::url() === url('privacy_settings'))
+      <li class="active">
+    @else
+      <li>
+    @endif
+      <a href=""><i class="fa fa-file" style=""></i> COLLECTION - COMING SOON</a>
+    </li>
+
+</ul>
