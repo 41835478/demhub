3ccbdc8aed0a20cbc907510d2391fad7b323f724
alfance <alfance.elric@gmail.com>
2015-11-13 16:07:52 -0500

    same as before

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,58 +9,66 @@
 @endsection
 
 @section('content')
-
+	<div class="row">
+		<div class="col-xs-12">
 			<div id="welcome_home" class="row js-landing-hero">
+
 				<div class="col-md-6 col-md-offset-6 lading-content-1">
 					{!! HTML::image("/images/logo/demhub_logo-05.svg", "DEMHUB logo", array('class' => 'img-responsive demhub-logo-landing', 'width' => '300')) !!}
 				<br></br>
 				<div class="col-md-6 col-md-offset-1 text-center">
 					<a type="button" class="btn btn-default btn-lg btn-style-w" href="{{url('divisions')}}">TRY THE BETA</a>
 				</div>
-				</div>
+				<a class="mouse-scroll" href="#welcome_about">
+				  <span class="mouse">
+				    <span class="mouse-movement">
+				    </span>
+				  </span>
+				</a>
+			</div>
 			</div>
 
 			<div class="" style="">
 				<div id="welcome_about" class="row">
 
-					<div class="col-md-12">
+					<div class="col-md-12" >
 						<h1>What is DEMHUB?</h1>
 						<p>DEMHUB is a networking platform built to facilitate the exchange of information, experience and resources for the Global Disaster and Emergency Management Industry.</p>
 					</div>
 
 					<div class="col-md-2">
 						<h3>News</h3>
-						<h2><i class="fa fa-newspaper-o"></i></h2>
+						<h2><i class="icon-news"></i></h2>
 						<p>Get the NEWS from reliable industry sources.</p>
 					</div>
 
 					<div class="col-md-2">
 						<h3>Discuss</h3>
-						<h2><i class="fa fa-comments"></i></h2>
+						<h2><i class="icon-discuss whiltefill"></i></h2>
 						<p>Exchange your thoughts with millions of industry professionals.</p>
 					</div>
 
 					<div class="col-md-2">
 						<h3>Connect</h3>
-						<h2><i class="fa fa-pencil-square-o"></i></h2>
+						<h2><i class="icon-users"></i></h2>
 						<p>Discover and get discovered within professional circles.</p>
 					</div>
 
 					<div class="col-md-2">
 						<h3>Research</h3>
-						<h2><i class="fa fa-files-o"></i></h2>
+						<h2><i class="icon-research"></i></h2>
 						<p>Explore the academic researches about disaster management as well as publish your own.</p>
 					</div>
 
 					<div class="col-md-2">
 						<h3>Share</h3>
-						<h2><i class="fa fa-pencil-square-o"></i></h2>
+						<h2><i class="icon-share"></i></h2>
 						<p>Upload documents, reports, articles, and more to share with your network.</p>
 					</div>
 
 					<div class="col-md-2">
 						<h3>Track</h3>
-						<h2><i class="fa fa-map-pin"></i></h2>
+						<h2><i class="icon-track"></i></h2>
 						<p>Track NEWS and discussions on issues that matter most to you.</p>
 					</div>
 
@@ -78,16 +86,17 @@
 						<a type="button" class="btn btn-default btn-lg btn-style-w" href="{{url('divisions')}}">SIX DIVISIONS</a>
 					</div>
 
+
 					<p>&nbsp;</p>
 					<p>&nbsp;</p>
 
-					<div class="row" style="margin-left: 15px; margin-right: 15px;padding:0px">
+					<div class="row" style="margin-left: 15px; margin-right: 15px;">
 						@foreach($divisions as $div)
 							<a href="{{url('division', $div->slug)}}">
 								<div class="division-landing-box col-md-2">
 									<div id="division_{{$div->id}}">
-										<div class="division-landing-icon">
-											<img src ="/images/icons/division-0{{$div->id}}.svg">
+										<div class="icon-{{$div->slug}}">
+											<!-- <img src ="/images/icons/division-0{{$div->id}}.svg"> -->
 										</div>
 										<h3 class="division-landing-name">{{$div->name}}</h3>
 									</div>
@@ -110,5 +119,5 @@
 
 			</div>
 		</div>
-
+	</div>
 @stop
