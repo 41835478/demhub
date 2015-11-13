afb9b89093be037f30d18e431903bbd7d4b03480
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2015-11-13 06:48:44 -0500

    Fuck this... I'm done

--- resources/views/frontend/user/dashboard/test.blade.php
+++ resources/views/frontend/user/dashboard/test.blade.php
@@ -5,23 +5,4 @@
   @include('frontend.user.dashboard._sidebar')
   {{-- @include('frontend.user.dashboard._dash_content') --}}
   @include('frontend.user.dashboard._dash_content_test')
-
-  {!! HTML::script("js/dashboardtest/utility.js") !!}
-  {!! HTML::script("js/dashboardtest/demo.js") !!}
-  {!! HTML::script("js/dashboardtest/main.js") !!}
-
-  <script type="text/javascript">
-    jQuery(document).ready(function() {
-
-      "use strict";
-
-      // Init Demo JS
-      Demo.init();
-
-
-      // Init Theme Core
-      Core.init();
-
-    });
-  </script>
 @endsection
