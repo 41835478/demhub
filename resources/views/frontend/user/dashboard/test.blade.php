4bca466c9d780073f3ab1880d717105e2b5425e1
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2015-11-13 07:01:33 -0500

    I have been enlightened... if this works I'm going to cry

--- resources/views/frontend/user/dashboard/test.blade.php
+++ resources/views/frontend/user/dashboard/test.blade.php
@@ -5,4 +5,23 @@
   @include('frontend.user.dashboard._sidebar')
   {{-- @include('frontend.user.dashboard._dash_content') --}}
   @include('frontend.user.dashboard._dash_content_test')
+  <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+  {!! HTML::script("js/dashboardtest/utility.js") !!}
+  {!! HTML::script("js/dashboardtest/demo.js") !!}
+  {!! HTML::script("js/dashboardtest/main.js") !!}
+
+  <script type="text/javascript">
+    jQuery(document).ready(function() {
+
+      "use strict";
+
+      // Init Demo JS
+      Demo.init();
+
+
+      // Init Theme Core
+      Core.init();
+
+    });
+  </script>
 @endsection
