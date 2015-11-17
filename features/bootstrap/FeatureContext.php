abfe5a69657c870c0607b74de470bf62b3bbc4d6
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2015-11-17 17:47:43 -0500

    Update behat tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -46,10 +46,72 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
     }
 
     /**
-     * @When I wait :seconds second(s)
+     * @When I register
      */
-    // public function iWaitSecond($seconds)
-    // {
-    //   $this->getSession()->wait(1000);
-    // }
+    public function iRegister()
+    {
+        // Check if user is in the registration page
+        $this->assertSession()->addressEquals($this->locatePath("/auth/register"));
+        // Fill in username
+        $field = $this->fixStepArgument("Username");
+        $value = $this->fixStepArgument("johndoe123");
+        $this->getSession()->getPage()->fillField($field, $value);
+        // Fill in email
+        $field = $this->fixStepArgument("email");
+        $value = $this->fixStepArgument("johndoe123@example.com");
+        $this->getSession()->getPage()->fillField($field, $value);
+        // Fill in password
+        $field = $this->fixStepArgument("password");
+        $value = $this->fixStepArgument("password123");
+        $this->getSession()->getPage()->fillField($field, $value);
+        // Fill in password confirmation
+        $field = $this->fixStepArgument("password_confirmation");
+        $value = $this->fixStepArgument("password123");
+        $this->getSession()->getPage()->fillField($field, $value);
+        // Click "Join"
+        $link = $this->fixStepArgument("JOIN");
+        $this->getSession()->getPage()->clickLink($link);
+        // Check for modal
+        $this->assertSession()->pageTextContains($this->fixStepArgument("We verify each member who signs up"));
+
+
+
+        // Click "Continue"
+        // $link = $this->fixStepArgument("CONTINUE");
+        // $this->getSession()->getPage()->clickLink($link);
+
+        $button = $this->getSession()->getPage()->findButton("CONTINUE");
+
+        // If not found throw an exception
+        if (null === $button) {
+            throw new \InvalidArgumentException('Element not found');
+        }
+
+        // Otherwise click on it
+        $button->click();
+
+
+
+
+
+        // Fill in First Name
+        $field = $this->fixStepArgument("first_name");
+        $value = $this->fixStepArgument("John");
+        $this->getSession()->getPage()->fillField($field, $value);
+        // Fill in Last Name
+        $field = $this->fixStepArgument("last_name");
+        $value = $this->fixStepArgument("Doe");
+        $this->getSession()->getPage()->fillField($field, $value);
+        // Fill in Job Title
+        $field = $this->fixStepArgument("job_title");
+        $value = $this->fixStepArgument("Cool Job");
+        $this->getSession()->getPage()->fillField($field, $value);
+        // Fill in Organization/Agency Name
+        $field = $this->fixStepArgument("organization_name");
+        $value = $this->fixStepArgument("Cool Organization");
+        $this->getSession()->getPage()->fillField($field, $value);
+        // Click "Done"
+        $link = $this->fixStepArgument("DONE");
+        $this->getSession()->getPage()->pressButton($link);
+    }
 }
