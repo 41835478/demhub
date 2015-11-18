722f8d17e6956bc673817e8add960977489c2d28
alfance <alfance.elric@gmail.com>
2015-11-18 18:21:13 -0500

    Fix minor issue regarding migrations

--- database/migrations/2015_11_12_234136_add_location_to_users_table.php
+++ database/migrations/2015_11_12_234136_add_location_to_users_table.php
@@ -25,7 +25,7 @@ class AddLocationToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('language');
+            $table->dropColumn('location');
         });
     }
 }
