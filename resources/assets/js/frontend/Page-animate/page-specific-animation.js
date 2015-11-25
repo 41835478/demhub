3c9948794cf76c9d3dcf400a0c1b02d0cf7a3315
kylo <alfance.elric@gmail.com>
2015-11-25 14:32:39 -0500

    division bar again

--- resources/assets/js/frontend/Page-animate/page-specific-animation.js
+++ resources/assets/js/frontend/Page-animate/page-specific-animation.js
@@ -1,6 +1,6 @@
 //1) For all pages that have division navigation, this fix the position of the bar when scrolling.
 
-<<<<<<< HEAD
+
 $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = 450;             // set to whatever you want it to be
@@ -16,20 +16,6 @@ $(window).on('scroll', function() {
       $('.divisions-page-item').removeClass('float-top');
     }
 });
-=======
-// $(window).on('scroll', function() {
-//     var y_scroll_pos = window.pageYOffset;
-//     var scroll_pos_test = 450;             // set to whatever you want it to be
-//
-//     if(y_scroll_pos > scroll_pos_test) {
-//       $('#welcome-division-bar').addClass('fix-division-bar ');
-//       $('.divisions-page-item').addClass('float-top');
-//     }
-//     else {
-//       $('#welcome-division-bar').removeClass('fix-division-bar ');
-//       $('.divisions-page-item').removeClass('float-top');
-//     }
-// });
->>>>>>> 7c4479eadf4161768e4c8e9b591e0b4f54f9c0e8
+
 
 //2)
