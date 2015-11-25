eb8bbdcecbd731ae6eccab13b8cbb3fb58721249
kylo <alfance.elric@gmail.com>
2015-11-25 13:26:57 -0500

    pull from kylo

--- resources/assets/js/frontend/Page-animate/page-specific-animation.js
+++ resources/assets/js/frontend/Page-animate/page-specific-animation.js
@@ -4,14 +4,16 @@ $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = 450;             // set to whatever you want it to be
 
+
     if(y_scroll_pos > scroll_pos_test) {
       $('#welcome-division-bar').addClass('fix-division-bar ');
       $('.divisions-page-item').addClass('float-top');
     }
+
     else {
       $('#welcome-division-bar').removeClass('fix-division-bar ');
       $('.divisions-page-item').removeClass('float-top');
     }
 });
 
-//1)
+//2)
