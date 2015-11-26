79e761208193947d19212b13f25ddf5c3280491d
lhaggarty <lhaggarty@me.com>
2015-11-26 13:57:14 -0500

    temp disable dropdown division menu

--- resources/assets/js/frontend/Page-animate/page-specific-animation.js
+++ resources/assets/js/frontend/Page-animate/page-specific-animation.js
@@ -1,21 +1,21 @@
 //1) For all pages that have division navigation, this fix the position of the bar when scrolling.
 
 
-$(window).on('scroll', function() {
-    var y_scroll_pos = window.pageYOffset;
-    var scroll_pos_test = 450;             // set to whatever you want it to be
-
-
-    if(y_scroll_pos > scroll_pos_test) {
-      $('#welcome-division-bar').addClass('fix-division-bar ');
-      $('.divisions-page-item').addClass('float-top');
-    }
-
-    else {
-      $('#welcome-division-bar').removeClass('fix-division-bar ');
-      $('.divisions-page-item').removeClass('float-top');
-    }
-});
+// $(window).on('scroll', function() {
+//     var y_scroll_pos = window.pageYOffset;
+//     var scroll_pos_test = 450;             // set to whatever you want it to be
+//
+//
+//     if(y_scroll_pos > scroll_pos_test) {
+//       $('#welcome-division-bar').addClass('fix-division-bar ');
+//       $('.divisions-page-item').addClass('float-top');
+//     }
+//
+//     else {
+//       $('#welcome-division-bar').removeClass('fix-division-bar ');
+//       $('.divisions-page-item').removeClass('float-top');
+//     }
+// });
 
 
 //2)
