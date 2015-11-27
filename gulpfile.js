113ac4d317f86a185e9c9ed7e1c4f3077cc4ae9a
kylo <alfance.elric@gmail.com>
2015-11-27 17:51:58 -0500

    last few changes to front page

--- gulpfile.js
+++ gulpfile.js
@@ -75,12 +75,6 @@ elixir(function(mix) {
         ]);
 });
 
-var gulp                = require('gulp'),
-svgSprite               = require('gulp-svg-sprite');
-
-gulp.src('path/to/assets/*.svg')
-    .pipe(svgSprite( /* ... Insert your configuration here ... */ ))
-    .pipe(gulp.dest('out'));
 
 /**
  * Uncomment for LESS version
