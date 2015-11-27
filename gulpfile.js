186a5898dbd2d026f28b07e8732d9c61aa962e8b
kylo <alfance.elric@gmail.com>
2015-11-27 14:34:40 -0500

    add icons sheet

--- gulpfile.js
+++ gulpfile.js
@@ -75,6 +75,13 @@ elixir(function(mix) {
         ]);
 });
 
+var gulp                = require('gulp'),
+svgSprite               = require('gulp-svg-sprite');
+
+gulp.src('path/to/assets/*.svg')
+    .pipe(svgSprite( /* ... Insert your configuration here ... */ ))
+    .pipe(gulp.dest('out'));
+
 /**
  * Uncomment for LESS version
  */
