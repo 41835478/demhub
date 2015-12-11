d875be733fc0b8502348c41bfa08c961154ca21d
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2015-12-11 11:12:04 -0500

    Add obversers to service provider

--- app/Providers/ObserversServiceProvider.php
+++ app/Providers/ObserversServiceProvider.php
@@ -1,7 +1,15 @@
 <?php namespace App\Providers;
 
 use App\Observers\ElasticsearchArticleObserver;
+use App\Observers\ElasticsearchDiscussionObserver;
+use App\Observers\ElasticsearchInfoResourceObserver;
+use App\Observers\ElasticsearchPublicationObserver;
+use App\Observers\ElasticsearchUserObserver;
 use App\Models\Article;
+use Riari\Forum\Repositories\Threads;
+use App\Models\InfoResource;
+use App\Models\Publication;
+use App\Models\Access\User\User;
 use Illuminate\Support\ServiceProvider;
 
 class ObserversServiceProvider extends ServiceProvider
@@ -9,6 +17,10 @@ class ObserversServiceProvider extends ServiceProvider
     public function boot()
     {
         Article::observe($this->app->make(ElasticsearchArticleObserver::class));
+        Threads::observe($this->app->make(ElasticsearchDiscussionObserver::class));
+        InfoResource::observe($this->app->make(ElasticsearchInfoResourceObserver::class));
+        Publication::observe($this->app->make(ElasticsearchPublicationObserver::class));
+        User::observe($this->app->make(ElasticsearchUserObserver::class));
     }
 
     public function register()
@@ -16,6 +28,22 @@ class ObserversServiceProvider extends ServiceProvider
         $this->app->bindShared(ElasticsearchArticleObserver::class, function()
         {
             return new ElasticsearchArticleObserver();
+        });
+        $this->app->bindShared(ElasticsearchDiscussionObserver::class, function()
+        {
+            return new ElasticsearchDiscussionObserver();
+        });
+        $this->app->bindShared(ElasticsearchInfoResourceObserver::class, function()
+        {
+            return new ElasticsearchInfoResourceObserver();
+        });
+        $this->app->bindShared(ElasticsearchPublicationObserver::class, function()
+        {
+            return new ElasticsearchPublicationObserver();
+        });
+        $this->app->bindShared(ElasticsearchUserObserver::class, function()
+        {
+            return new ElasticsearchUserObserver();
         });
     }
 }
