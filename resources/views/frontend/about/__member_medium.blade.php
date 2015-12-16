12ff0862bf1d78749685db13efec2464ff7198ad
lhaggarty <lhaggarty@me.com>
2015-12-16 17:58:51 -0500

    update stuff

--- resources/views/frontend/about/__member_medium.blade.php
+++ resources/views/frontend/about/__member_medium.blade.php
@@ -2,7 +2,7 @@
 ================================= -->
 <div class="col-lg-3 col-md-3 col-sm-6">
   <div class="member">
-    {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "mask-medium"]) !!}
+    {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-rounded"]) !!}
     <h3 style="font-weight: bold;">{{ $name }}</h3>
     <h4 style="font-weight: lighter;">{{ $position }}</h4>
     <h5 style="">{{ $description }}</h5>
--- resources/views/frontend/user/dashboard/connections_partials/_connections.blade.php
+++ resources/views/frontend/user/dashboard/connections_partials/_connections.blade.php
@@ -8,38 +8,47 @@
 
 
             <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class = "feedsbox">
+            <div class = "peoplebox">
 
 
               <div class ="color-label division_all"></div>
               <div class="inner-feedsbox" style="text-align:center">
-              <div class="row">
-              <div class="col-xs-6" >
+                <div class="member" style="margin: 0 auto;width:150px;padding-top:15px">
+
+                  {!! HTML::image($user->avatar->url('medium'), '$user->avatar_file_name', ['class' => "img-rounded img-responsive"]) !!}
+                </div>
               <a href="{{ URL::to('profile/' . $user->user_name) }}">
                 <h3>  {{$user->first_name}} {{$user->last_name}}  </h3>
               </a>
 
 
-                <div class="member">
-
-                  {!! HTML::image($user->avatar->url('medium'), '$user->avatar_file_name', ['class' => "mask-medium"]) !!}
-                </div>
-              <!-- <span class="label label-default" style="font-size:82%">
-              </span> -->
-            </div>
-            <div class="col-xs-6" style="padding-top:50px">
+            <div>
               <p>
                 {{$user->job_title}}
               </p>
               <p>
+                @if ($user->organization_name)
+                  at
+                @else
+                @endif
                 {{$user->organization_name}}
               </p>
-              <p>
+              <span style="color:#999">
                 {{$user->location}}
-              </p>
-
+              </span>
             </div>
-          </div>
+
+            <div style="margin: 0 auto;width:100%; height:40px; bottom:0px; position:absolute;">
+
+               <button type="button" class="btn btn-greytone btn-sm" style="margin-left:5px;"  aria-haspopup="true" aria-expanded="false">
+                 <div class="glyphicon glyphicon-plus" aria-hidden="true"> FOLLOW</div>
+               </button>
+
+               <a type="button" class="btn btn-style-alt btn-sm" href="mailto:{{$user->email}}?Subject=DEMHUB%20Connection" target="_top">
+                 <div class="glyphicon glyphicon-envelope" aria-hidden="true"> Email</div>
+               </a>
+            </div>
+
 
             </div>
           </div> <!-- the div that closes the box -->
