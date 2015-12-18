7b032d4037e3a64dff8820cc508453761a2c9188
lhaggarty <lhaggarty@me.com>
2015-12-17 19:30:04 -0500

    update

--- resources/views/frontend/user/dashboard/my_publication/view.blade.php
+++ resources/views/frontend/user/dashboard/my_publication/view.blade.php
@@ -21,7 +21,7 @@
 
                 <h3 style="text-align:center">{{ $publication->title }}</h3>
 
-                <embed class="" src="{{ $publication->document->url() }}" style="width:175%;height:175%;max-width:600px;max-height:1000px;"></embed>
+                <embed class="img-responsive" src="{{ $publication->document->url() }}" style="width:175%;height:750px;max-width:600px;max-height:1000px;"></embed>
 
 
     </div>
