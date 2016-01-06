23a1c332f7bc45d822e9ed601e46f2a74bc79407
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-01-06 15:43:29 -0500

    Patched riari forum package, threads now observable

---
+++ app/Console/Commands/IndexDiscussionsToElasticsearch.php
@@ -0,0 +1,68 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Riari\Forum\Models\Thread;
+use Es;
+
+class IndexDiscussionsToElasticsearch extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $name = "es:discussions-index";
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $description = "Indexes all discussions (threads) to elasticsearch";
+
+    /**
+     * @return void
+     */
+    public function fire()
+    {
+        // Uncomment if index needs to be created
+        $indexParams = [
+            'index' => 'discussions'
+        ];
+
+        // Es::indices()->delete($indexParams);
+        if (!Es::indices()->exists($indexParams)) {
+            Es::indices()->create($indexParams);
+        }
+
+        $mappingProperties = [
+            'index' => 'discussions',
+            'type' => 'threads',
+            'body' => [
+                'threads' => [
+                    '_source' => [
+                        'enabled' => true
+                    ]
+                    // ,
+                    // 'properties' => [
+                    //     'publish_date' => [
+                    //         'type' => 'date',
+                    //         'format' => 'yyyy-MM-dd HH:mm:ss'
+                    //     ]
+                    // ]
+                ]
+            ]
+        ];
+        Es::indices()->putMapping($mappingProperties);
+
+        Thread::chunk(100, function($threads) {
+            foreach ($threads as $thread) {
+                $params = [
+                    'index' => 'discussions',
+                    'type' => 'threads',
+                    'id' => $thread->id,
+                    'body' => $thread['attributes']//alternative to $thread->toArray()
+                ];
+                Es::index($params);
+            }
+        });
+
+    }
+
+}
