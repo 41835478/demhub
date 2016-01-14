da38fbc89b0a86049b76db5edd1e90818dbee9da
lhaggarty <lhaggarty@me.com>
2016-01-13 21:00:27 -0500

    tag visual on news

--- resources/views/division/__keyword-dropup-foreach.blade.php
+++ resources/views/division/__keyword-dropup-foreach.blade.php
@@ -6,15 +6,18 @@
   <a class="label label-default" style="font-size:82%;margin-right:2px" href="/?query_term={{$keyword}}">
     {{ $keyword }}
   </a>
+  <div class="arrows" style="display:inline-block;margin-left:-45px;"><i class="" ></i></div>
+
   @elseif($index==2)
   {{-- Creates a keyword dropup menu  AND adds second keyword to the dropup--}}
-  <div class="dropup" style="display:inline">
+  <div class="dropup" style="display:inline;margin-left:-45px">
     <a type="button" class="label label-default dropdown-toggle"
     data-toggle="dropdown" aria-haspopup="true" id="dropdownMenu2" aria-expanded="false"
     style="font-size:82%;margin-right:2px">
     and {{count($keywords)}} other keywords
       <span class="caret"></span>
     </a>
+    <div class="arrows" style="display:inline-block;margin-left:-45px;"><i class="triangle-right" ></i></div>
     <ul class="dropdown-menu label-default" aria-labelledby="dropdownMenu2">
       <li><a href="?query_term={{$keyword}}">{{$keyword}}</a></li>
     @elseif($index>2)
