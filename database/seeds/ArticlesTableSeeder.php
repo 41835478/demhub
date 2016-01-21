1da798074b21caf6e4c4249674fef48f648f8bb5
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-01-21 18:49:31 -0500

    Finalize seed files with new STI architecture

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use League\Csv\Reader;
-use Carbon\Carbon as Carbon;
+use Carbon\Carbon;
 
-class InfoResourceTableSeeder extends Seeder
+class ArticlesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,24 +13,32 @@ class InfoResourceTableSeeder extends Seeder
      */
     public function run()
     {
-        $resourcesData = database_path().'/data/info_resources.csv';
-        $csv = Reader::createFromPath($resourcesData);
+        $articlesData = database_path().'/data/articles.csv';
+        $csv = Reader::createFromPath($articlesData);
         // $csv->setOffset(1) // start from the 2nd row, since the 1st is the CSV header
 
         $nb_iterations = $csv->setOffset(1)->fetchAll(function ($row) {
             DB::table('contents')->insert([
                 // 'id' => ..., // id is assigned automatically, so we don't need it
-                'subclass'      => 'infoResource', // $row[0]
+                'subclass'      => 'article', // $row[0]
                 'name'          => $row[1] == "NULL" ? NULL : $row[1],
+                'description'   => $row[2] == "NULL" ? NULL : $row[2],
+                'data'          => $row[3] == "NULL" ? NULL : $row[3],
                 'divisions'     => $row[4] == "NULL" ? NULL : $row[4],
                 'keywords'      => $row[5] == "NULL" ? NULL : $row[5],
                 'url'           => $row[7] == "NULL" ? NULL : $row[7],
                 'country'       => $row[8] == "NULL" ? NULL : $row[8],
                 'state'         => $row[9] == "NULL" ? NULL : $row[9],
+                'city'          => $row[10] == "NULL" ? NULL : $row[10],
+                'lat'           => $row[11] == "NULL" ? NULL : $row[11],
+                'lng'           => $row[12] == "NULL" ? NULL : $row[12],
                 'visibility'    => $row[15] == "NULL" ? NULL : $row[15],
+                'status_flag'   => $row[16] == "NULL" ? NULL : $row[16],
+                'owner_id'      => $row[17] == "NULL" ? NULL : $row[17],
                 'deleted'       => $row[18] == "NULL" ? NULL : $row[18],
-                'created_at'    => Carbon::now(),
-                'updated_at'    => Carbon::now()
+                'publish_date'  => $row[19] == "NULL" ? NULL : Carbon::parse($row[19]),
+                'created_at'    => $row[20] == "NULL" ? NULL : Carbon::parse($row[20]),
+                'updated_at'    => $row[21] == "NULL" ? NULL : Carbon::parse($row[21])
             ]);
         });
     }
