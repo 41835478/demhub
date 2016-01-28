209306f875f33d260ab2b29ea28eb5747c676f0c
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-01-28 18:06:09 -0500

    Fix minor backend issues

--- resources/views/frontend/search/_publication_results.blade.php
+++ resources/views/frontend/search/_publication_results.blade.php
@@ -11,12 +11,11 @@
       <td>
 
         <a href="{{ URL::to('publication/' . $publication['id'] . '/view') }}">
-          {{ $publication['title'] }}
+          {{ $publication['name'] }}
         </a>
         {{-- <span class="fa fa-circle text-info fs14 mr10"></span> --}}
-
       </td>
-      <td class="text-muted text-right">{{ $publication['publication_author'] }}</td>
+      <td class="text-muted text-right">{{ \App\Http\Components\Helpers::author($publication) }}</td>
     </tr>
   @empty
     <br>No publications to show<br>
