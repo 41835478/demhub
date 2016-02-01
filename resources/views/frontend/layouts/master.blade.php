5541113b615183ebe13a4b0c6fe789b7b616af4d
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-02-01 14:35:53 -0500

    Continue refactoring and moving files around

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -12,27 +12,11 @@
     <meta name="google-site-verification" content="vVSYl3mhbDJShVxNX9St2jNw1h6sKkHaz1IgTEKC5xs" />
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
-    {{-- <script src="build/js/jquery-1.11.3.min.js"></script>
-    <script src="build/js/bootstrap.min.js"></script> --}}
-
     @yield('meta')
     @yield('before-styles-end')
     {!! HTML::style(elixir('css/core.css')) !!}
     {!! HTML::style(elixir('css/frontend.css')) !!}
-
     @yield('after-styles-end')
-
-    <?php // "coming soon" logic
-      $pattern = "/^(http(s?):\/\/)?((((staging)|(beta)).demhub.net)|(localhost:8000)|(demhub.dev))\/?(.+)?$/i";
-      $today = date("Y-m-d H:i:s");
-      $launch_date = "2015-11-10 00:00:00";
-      // FALSE if deploying to production // TRUE if testing on development
-      $display_coming_soon = preg_match($pattern, Request::url()) == FALSE && $today < $launch_date;
-    ?>
-
-    <!-- @if(Request::url() == url('dashboard') || strpos(Request::url(), "publication")!==false || Request::url()==url('connections'))
-      {!! HTML::style(elixir('css/dashboardtest.css')) !!}
-    @endif -->
 
     <!-- Fonts -->
     <link href="//fonts.googleapis.com/css?family=Roboto:400,300" rel="stylesheet" type="text/css">
@@ -41,8 +25,6 @@
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <!-- Place favicon.ico in the root directory -->
 
-
-
   </head>
 
   <body class="@yield('body-class')">
@@ -50,34 +32,29 @@
         <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
 
+    <div class="wrapper">
+      @if (Auth::user())
+        @include('frontend.includes._feedback_sidebar')
+        @include('modals._feedback_thankyou')
+      @endif
 
-    @if($display_coming_soon)
-      @include('frontend.includes._coming-soon')
-    @else
-      <div class="wrapper">
-        @if (Auth::user())
-          @include('frontend.includes._feedback_sidebar')
-          @include('modals._feedback_thankyou')
-        @endif
+      @include('frontend.navigation._navigation')
+      @include('includes.partials.messages')
+      <div
+      @if(Request::url() == url('dashboard') || Request::url() == url('') || strpos(Request::url(), "publication")!==false || Request::url()==url('connections'))
+      class="@yield('container-class') container-fluid"
+      @else
+      class="@yield('container-class')"
+      @endif
+      style="overflow-x:hidden;">
 
-        @include('frontend.navigation._navigation')
-        @include('includes.partials.messages')
-        <div
-        @if(Request::url() == url('dashboard') || Request::url() == url('') || strpos(Request::url(), "publication")!==false || Request::url()==url('connections'))
-        class="@yield('container-class') container-fluid"
-        @else
-        class="@yield('container-class')"
-        @endif
-        style="overflow-x:hidden;">
+        @yield('body-style')
+        @yield('content')
 
-          @yield('body-style')
-          @yield('content')
-
-        <div class="push"></div>
-      </div><!-- ./container-fluid -->
-      </div><!-- ./wrapper -->
-      @include('frontend.includes._footer')
-    @endif
+      <div class="push"></div>
+    </div><!-- ./container-fluid -->
+    </div><!-- ./wrapper -->
+    @include('frontend.includes._footer')
 
     @yield('modal')
 
