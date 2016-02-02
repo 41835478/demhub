6105154adfdb9b81c30e2923d268d5a0ce185c73
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-02-01 19:21:09 -0500

    Add new logic for summary and teaser card'

---
+++ resources/views/frontend/includes/_card.blade.php
@@ -0,0 +1,79 @@
+<?php // NOTE - Logic to handle items by type, class, subclass and view
+  if (!is_array($item) && get_class($item) == 'content') {
+    $array = [
+      'id'            => $item->id,
+      'subclass'      => $item->subclass,
+      'name'          => $item->name,
+      'description'   => $item->description,
+      'divisions'     => $item->divisions(),
+      'keywords'      => $item->keywords(),
+      'slug'          => $item->slug,
+      'url'           => $item->url,
+      'country'       => $item->country,
+      'state'         => $item->state,
+      'city'          => $item->city,
+      'lat'           => $item->lat,
+      'lng'           => $item->lng,
+      'pinned_by'     => $item->pinned_by,
+      'pinned_at'     => $item->pinned_at,
+      'visibility'    => $item->visibility,
+      'status_flag'   => $item->status_flag,
+      'owner_id'      => $item->owner_id,
+      'deleted'       => $item->deleted,
+      'publish_date'  => $item->publish_date,
+      'created_at'    => $item->created_at,
+      'updated_at'    => $item->updated_at,
+    ];
+
+    switch ($item->subclass) {
+      case 'article':
+        $array = [
+          'language' => $item->language(),
+          'type' => $item->type()
+        ];
+        break;
+
+      case 'infoResource':
+        // nothing
+        break;
+
+      case 'publication':
+        $array = [
+          'volume'      => $item->volume(),
+          'issues'      => $item->issues(),
+          'pages'       => $item->pages(),
+          'publisher'   => $item->publisher(),
+          'institution' => $item->institution(),
+          'conference'  => $item->conference(),
+          'author'      => $item->author(),
+          'favorites'   => $item->favorites(),
+          'views'       => $item->views()
+        ];
+        break;
+
+      case 'thread':
+        $array = [
+          'view_count' => $item->view_count
+        ];
+        break;
+
+      default:
+        // nothing
+        break;
+    }
+  }
+?>
+
+@if((!is_array($item) && get_class($item) == 'content') || (is_array($item) && $item['subclass']))
+  @if($type == 'teaser')
+    @include('frontend.__content-teaser')
+  @else
+    @include('frontend.__content-card')
+  @endif
+@else
+  @if($type == 'teaser')
+    @include('frontend.user.__user-teaser')
+  @else
+    @include('frontend.user.__user-card-partial')
+  @endif
+@endif
