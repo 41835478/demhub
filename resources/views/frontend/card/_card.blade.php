63f7cc3462945bb637ddd796f228bca9ad3ee2ef
lhaggarty <lhaggarty@me.com>
2016-02-03 12:50:16 -0600

    testing search page

--- resources/views/frontend/card/_card.blade.php
+++ resources/views/frontend/card/_card.blade.php
@@ -66,16 +66,30 @@ if (!is_array($item) && get_class($item) == 'content') {
     }
   }
   // Elastic search result
-  elseif(! empty($item) || is_array($item)) {
+  elseif(! empty($item) || ($item['subclass']=='infoResource')) {
       $divisions = array();
-      foreach (Helpers::convertDBStringToArray($item['divisions']) as $divID) {
-          $div = Division::findOrFail($divID);
+      dd($item['divisions']);
+      $divSlugs=str_replace(' ', '', $item['divisions']);
+      $divSlugs=strtolower($divSlugs);
+      $divSlugs=array_filter(preg_split("/\,/", $divSlugs));
+      foreach ($divSlugs as $divSlug) {
+          $div = Division::where('slug','=',$divSlug)->first();
           $divisions[$div->slug] = $div->name;
       }
       $item['divisions'] = $divisions;
       $item['keywords'] = Helpers::convertDBStringToArray($item['keywords']);
 
   }
+  elseif(! empty($item) || is_array($item)) {
+     $divisions = array();
+     foreach (Helpers::convertDBStringToArray($item['divisions']) as $divID) {
+         $div = Division::findOrFail($divID);
+         $divisions[$div->slug] = $div->name;
+     }
+     $item['divisions'] = $divisions;
+     $item['keywords'] = Helpers::convertDBStringToArray($item['keywords']);
+
+ }
 ?>
 
 @if((!is_array($item) && get_class($item) == 'content') || (isset($item['subclass'])))
