675d7814e09d71b04b9359bdb153c18cecd01a05
lhaggarty <lhaggarty@me.com>
2016-02-04 18:27:52 -0600

    slight diff

--- resources/views/division/_feeds.blade.php
+++ resources/views/division/_feeds.blade.php
@@ -12,9 +12,9 @@
   <div class="col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8">
   <div class="row">
     @foreach($newsFeeds as $item)
-      <div class="">
+
         @include('frontend.card._card')
-      </div>
+
     @endforeach
   </div>
 </div>
