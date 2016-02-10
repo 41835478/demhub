12df2c2dcbf756bb1f2f46ed59ed4da25b9937e6
lhaggarty <lhaggarty@me.com>
2016-02-10 17:37:18 -0500

    float

--- resources/views/frontend/about/__member_medium.blade.php
+++ resources/views/frontend/about/__member_medium.blade.php
@@ -2,7 +2,7 @@
 ================================= -->
 <div class="col-lg-4 col-md-4 col-sm-6">
 
-      <div class="box" style="height:365px;float:center !important">
+      <div class="box" style="height:365px;float:initial">
     {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle img-responsive", 'style' => 'max-width:100%;max-height:250px;margin: 0 auto']) !!}
     <h3 style="{{ $imgName == "JenniferD" ? 'font-size:167%;font-weight: bold' : 'font-weight: bold;' }}">{{ $name }}</h3>
     <h4 style="{{ $imgName == "SeanK" ? 'font-size:117%;font-weight: lighter' : 'font-weight: lighter;' }}">{{ $position }}</h4>
