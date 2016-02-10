79e689281be36bce737151c8fc010590a1048eca
lhaggarty <lhaggarty@me.com>
2016-02-10 17:35:02 -0500

    about style update again

--- resources/views/frontend/about/__member_medium.blade.php
+++ resources/views/frontend/about/__member_medium.blade.php
@@ -1,8 +1,8 @@
 <!-- MEMBER M
 ================================= -->
-<div class="col-lg-3 col-md-4 col-sm-6">
+<div class="col-lg-4 col-md-4 col-sm-6">
 
-      <div class="box" style="height:365px;float:center">
+      <div class="box" style="height:365px;float:center !important">
     {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle img-responsive", 'style' => 'max-width:100%;max-height:250px;margin: 0 auto']) !!}
     <h3 style="{{ $imgName == "JenniferD" ? 'font-size:167%;font-weight: bold' : 'font-weight: bold;' }}">{{ $name }}</h3>
     <h4 style="{{ $imgName == "SeanK" ? 'font-size:117%;font-weight: lighter' : 'font-weight: lighter;' }}">{{ $position }}</h4>
