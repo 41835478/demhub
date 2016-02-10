f95339bd567fefe71f41b6dbbb70f8f26e6b0a75
lhaggarty <lhaggarty@me.com>
2016-02-10 16:56:31 -0500

    new about page

--- resources/views/frontend/about/__member_medium.blade.php
+++ resources/views/frontend/about/__member_medium.blade.php
@@ -1,10 +1,12 @@
 <!-- MEMBER M
 ================================= -->
-<div class="col-lg-3 col-md-3 col-sm-6">
+<div class="col-lg-3 col-md-4 col-sm-6">
   <div class="member">
-    {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle"]) !!}
-    <h3 style="font-weight: bold;">{{ $name }}</h3>
+      <div class="box">
+    {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle", 'style' => 'width:250px;height:250px']) !!}
+    <h3 {{ $imgName == "JenniferD" ? 'style="font-size:90%;font-weight: bold"' : 'style="font-weight: bold;"' }}>{{ $name }}</h3>
     <h4 style="font-weight: lighter;">{{ $position }}</h4>
-    <h5 style="">{{ $description }}</h5>
+    {{-- <h5 style="">{{ $description }}</h5> --}}
+      </div>
   </div>
 </div>
--- resources/views/frontend/about/_description.blade.php
+++ resources/views/frontend/about/_description.blade.php
@@ -1,6 +1,6 @@
 <!-- DESCRIPTION
 ================================= -->
-<section id="about-description" class="section" style="min-height: 500px; background-color: #536b77; color: white; font-size: 20px; padding: 100px 0;">
+<section id="about-description" class="section" style="min-height: 200px; background-color: #536b77; color: white; font-size: 20px; padding: 40px 0;">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1">
@@ -14,7 +14,9 @@
 					</p>
 					<p>
 						We are committed to everything we do as it impacts more than just our team, it impacts our communities. So we make sure we're reliable, collaborative, and inclusive, providing the best platform for networking and knowledge sharing.
+
 					</p>
+                    <a href="mailto:info@demhub.net?Subject=DEMHUB%20Inquiry" target="_top" style="color:#38a7de">info@demhub.net</a>
 				</div>
 
 			</div>
