e4c3c8df26663cfe2a8d1b9ff2048d28d549e3ad
lhaggarty <lhaggarty@me.com>
2016-02-10 17:16:24 -0500

    change about page box size

--- resources/views/frontend/about/__member_medium.blade.php
+++ resources/views/frontend/about/__member_medium.blade.php
@@ -2,10 +2,10 @@
 ================================= -->
 <div class="col-lg-3 col-md-4 col-sm-6">
   <div class="member">
-      <div class="box">
+      <div class="box" style="height:365px">
     {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle", 'style' => 'width:250px;height:250px']) !!}
-    <h3 style="{{ $imgName == "JenniferD" ? 'font-size:167%;font-weight: bold;padding-bottom:1px' : 'font-weight: bold;' }}">{{ $name }}</h3>
-    <h4 style="{{ $imgName == "SeanK" ? 'font-size:117%;font-weight: lighter;padding-bottom:2px' : 'font-weight: lighter;' }}">{{ $position }}</h4>
+    <h3 style="{{ $imgName == "JenniferD" ? 'font-size:167%;font-weight: bold' : 'font-weight: bold;' }}">{{ $name }}</h3>
+    <h4 style="{{ $imgName == "SeanK" ? 'font-size:117%;font-weight: lighter' : 'font-weight: lighter;' }}">{{ $position }}</h4>
     {{-- <h5 style="">{{ $description }}</h5> --}}
       </div>
   </div>
