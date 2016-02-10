a1a71632e179ca7f4bd8fbf725592be0cddb805c
lhaggarty <lhaggarty@me.com>
2016-02-10 17:29:43 -0500

    about page responsive update

--- resources/views/frontend/about/__member_medium.blade.php
+++ resources/views/frontend/about/__member_medium.blade.php
@@ -1,12 +1,12 @@
 <!-- MEMBER M
 ================================= -->
 <div class="col-lg-3 col-md-4 col-sm-6">
-  <div class="member">
-      <div class="box" style="height:365px">
-    {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle", 'style' => 'width:250px;height:250px']) !!}
+
+      <div class="box" style="height:365px;float:center">
+    {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle img-responsive", 'style' => 'max-width:100%;max-height:250px;margin: 0 auto']) !!}
     <h3 style="{{ $imgName == "JenniferD" ? 'font-size:167%;font-weight: bold' : 'font-weight: bold;' }}">{{ $name }}</h3>
     <h4 style="{{ $imgName == "SeanK" ? 'font-size:117%;font-weight: lighter' : 'font-weight: lighter;' }}">{{ $position }}</h4>
-    {{-- <h5 style="">{{ $description }}</h5> --}} 
+    {{-- <h5 style="">{{ $description }}</h5> --}}
       </div>
-  </div>
+
 </div>
