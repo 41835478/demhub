a4548838a2d03efc68bcc9f1f5a62b5174841a52
lhaggarty <lhaggarty@me.com>
2016-02-10 17:19:26 -0500

    check

--- resources/views/frontend/about/__member_medium.blade.php
+++ resources/views/frontend/about/__member_medium.blade.php
@@ -6,7 +6,7 @@
     {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle", 'style' => 'width:250px;height:250px']) !!}
     <h3 style="{{ $imgName == "JenniferD" ? 'font-size:167%;font-weight: bold' : 'font-weight: bold;' }}">{{ $name }}</h3>
     <h4 style="{{ $imgName == "SeanK" ? 'font-size:117%;font-weight: lighter' : 'font-weight: lighter;' }}">{{ $position }}</h4>
-    {{-- <h5 style="">{{ $description }}</h5> --}}
+    {{-- <h5 style="">{{ $description }}</h5> --}} 
       </div>
   </div>
 </div>
