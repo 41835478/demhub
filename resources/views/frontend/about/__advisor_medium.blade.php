8acc02e8beebff62c839af1835784ff8b0774f60
lhaggarty <lhaggarty@me.com>
2016-02-11 13:10:50 -0500

    update photo

--- resources/views/frontend/about/__advisor_medium.blade.php
+++ resources/views/frontend/about/__advisor_medium.blade.php
@@ -3,7 +3,7 @@
 <div class="col-lg-4 col-md-4 col-sm-6">
 
       <div class="box" style="height:365px;float:initial">
-    {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle img-responsive", 'style' => 'min-width:250px;min-height:250px;max-width:100%;max-height:250px;margin: 0 auto']) !!}
+    {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle img-responsive", 'style' => 'max-width:100%;max-height:250px;margin: 0 auto']) !!}
     <h3 style="{{ $imgName == "JenniferD" ? 'font-size:167%;font-weight: bold' : 'font-weight: bold;' }}">{{ $name }}</h3>
     <h4 style="font-weight: lighter">{{ $position }}</h4>
     {{-- {{ $imgName == "SeanK" ? 'font-size:117%;font-weight: lighter' : 'font-weight: lighter;' }} --}}
