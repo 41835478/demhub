19d4067149af7c8f0914e6dddbbc4d1198ca4f23
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-02-16 15:32:22 -0500

    Refactoring of email

--- resources/views/forms/user/invite_people.blade.php
+++ resources/views/forms/user/invite_people.blade.php
@@ -1,34 +1,37 @@
-
-      <div class="modal-body" style="text-align:center;" id="inviteForm">
-        {!! Form::open(['route' => 'invite_others', 'class' => 'omb_loginForm', 'role' => 'form','data-toggle'=>'validator', 'data-delay'=>'1100', 'enctype'=>"multipart/form-data"]) !!}
+<div class="modal-body" style="text-align:center;" id="inviteForm">
+    {!! Form::open([
+        'route' => 'invite_others', 'class' => 'omb_loginForm',
+        'role' => 'form','data-toggle'=>'validator', 'data-delay'=>'1100',
+        'enctype'=>"multipart/form-data"
+    ]) !!}
         <div class="row" id="modal-body-row">
-
-          <h3>Invite Others To DEMHUB</h3>
-          <div class="form-group">
-            <div class="input-group">
-              <span class="input-group-addon"><i class="fa fa-user"></i></span>
-              {!! Form::input('0.email', '0.email', Input::get('0.email'), ['class' => 'form-control', 'placeholder' => 'Email Address','required','data-error'=> 'Invalid email address']) !!}
+            <h3>Invite Others To DEMHUB</h3>
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    {!! Form::input('0.email', '0.email', Input::get('0.email'), ['class' => 'form-control', 'placeholder' => 'Email Address','required','data-error'=> 'Invalid email address']) !!}
+                </div>
+                <div class="help-block with-errors"></div>
             </div>
-            <div class="help-block with-errors"></div>
-          </div>
+        </div>
 
-        </div>
         <div class="row" style="padding-bottom:10px">
-          <a class="btn btn-style-alt" onclick="addInviteForm()">Add Another</a>
+            <a class="btn btn-style-alt" onclick="addInviteForm()">Add Another</a>
         </div>
+
         <div class="row">
-          {!! Form::submit('DONE', ['class' => 'btn btn-lg btn-style-alt btn-block']) !!}
-          {!! Form::close() !!}
+            {!! Form::submit('DONE', ['class' => 'btn btn-lg btn-style-alt btn-block']) !!}
         </div>
-      </div>
+    {!! Form::close() !!}
+</div>
 
 
 <script>
-var formCounter=1;
-var formHtml='';
-function addInviteForm (){
-  formHtml='<div class="form-group"><div class="input-group"><span class="input-group-addon"><i class="fa fa-user"></i></span><input name="'+formCounter+'_email" class="form-control" placeholder="Email Address" required data-error="Invalid email address"></div><div class="help-block with-errors"></div></div>';
-  $("#modal-body-row").append(formHtml);
-  formCounter++;
-}
+    var formCounter=1;
+    var formHtml='';
+    function addInviteForm (){
+        formHtml='<div class="form-group"><div class="input-group"><span class="input-group-addon"><i class="fa fa-user"></i></span><input name="'+formCounter+'_email" class="form-control" placeholder="Email Address" required data-error="Invalid email address"></div><div class="help-block with-errors"></div></div>';
+        $("#modal-body-row").append(formHtml);
+        formCounter++;
+    }
 </script>
