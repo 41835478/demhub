a3d06eeffb007ee933c0c0d602588044998c5d90
lhaggarty <lhaggarty@me.com>
2016-02-22 16:24:04 -0500

    add hyerlink

--- resources/views/frontend/about/_advisor.blade.php
+++ resources/views/frontend/about/_advisor.blade.php
@@ -3,8 +3,9 @@
 <div class="col-lg-4 col-md-4 col-sm-6">
 
     <div class="box" style="height:365px;float:initial">
-        {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle img-responsive", 'style' => 'max-width:100%;max-height:250px;margin: 0 auto']) !!}
         <a  <?php echo (! empty($user_name)) ? 'href="'.url('profile/'.$user_name).'"' : null; ?>>
+            {!! HTML::image("images/about/team/".$imgName.".jpg", $name, ['class' => "img-circle img-responsive", 'style' => 'max-width:100%;max-height:250px;margin: 0 auto']) !!}
+
             <h3 style="{{ $imgName == "JenniferD" ? 'font-size:167%;font-weight: bold' : 'font-weight: bold;' }}">{{ $name }}</h3>
         </a>
         <h4 style="font-weight: lighter">{{ $position }}</h4>
